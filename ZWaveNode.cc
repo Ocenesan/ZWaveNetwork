beb9d2370e5d7a2a4d6329c0ac8fb08519b212c6
Ocenesan <125453542+Ocenesan@users.noreply.github.com>
2024-11-14 07:23:46 +0700

    Update ZWaveNode.cc

--- ZWaveNode.cc
+++ ZWaveNode.cc
@@ -1,5 +1,6 @@
 #include <omnetpp.h>
 #include <cmath>
+#include <string>
 
 using namespace omnetpp;
 
@@ -12,6 +13,7 @@ protected:
     double range;
     double frequencyLow;
     double frequencyHigh;
+    std::string deviceType;
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
 };
@@ -24,19 +26,33 @@ void ZWaveNode::initialize()
     range = par("range").doubleValue();
     frequencyLow = par("frequencyLow").doubleValue();
     frequencyHigh = par("frequencyHigh").doubleValue();
+    std::string deviceType = par("deviceType").stringValue();
     //EV << "ZWave Node " << getIndex() << " initialized with frequency " << frequency << " MHz and range " << range << " m\n";
-    EV << "ZWave Node " << getIndex() << " initialized with FSK frequencies "
-           << frequencyLow << " MHz and " << frequencyHigh << " MHz\n";
+    EV << "ZWave Node " << getIndex() << " (" << deviceType << ") initialized with frequencies "
+       << frequencyLow << " MHz and " << frequencyHigh << " MHz\n";
+    getDisplayString().setTagArg("t", 0, deviceType.c_str());
 }
 
 void ZWaveNode::handleMessage(cMessage *msg)
 {
     // Process the incoming message
+    std::string deviceType = par("deviceType").stringValue();
     double receivedFrequency = msg->par("frequency").doubleValue();
     int receivedBit = (receivedFrequency == frequencyLow) ? 0 : 1;
 
-    EV << "Node " << getIndex() << " received bit " << receivedBit << " with frequency "
-       << receivedFrequency << " MHz\n";
+    //EV << "Node " << getIndex() << " received bit " << receivedBit << " with frequency "
+       //<< receivedFrequency << " MHz\n";
+
+    if (deviceType == "Lamp") {
+        EV << "Lamp received bit " << receivedBit << "\n";
+        // Simulate light on/off state change here
+    } else if (deviceType == "Thermostat") {
+        EV << "Thermostat adjusting temperature based on received bit " << receivedBit << "\n";
+        // Simulate thermostat adjustment here
+    } else if (deviceType == "Door Lock") {
+        EV << "Door Lock received bit " << receivedBit << "\n";
+        // Simulate lock/unlock here
+    }
 
     // Forward the message to another random node, if desired
     int numGates = gateSize("gate");
